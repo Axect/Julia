ebcead1bcfee59736ace20da6d4cf0898f002455
Axect <edeftg@gmail.com>
2017-08-11 19:26:32 +0900

    Little Fix PBH

--- PBH/pbh.jl
+++ PBH/pbh.jl
@@ -5,7 +5,7 @@ const MZ = 91.1876
 const MH = 125.09
 const alphasMZ = 0.1182
 const h = 1E-04
-const step = 1E+04 * 44
+const N = 44
 
 mutable struct RGE
     t::Float64
@@ -18,7 +18,21 @@ mutable struct RGE
     G::Float64
 end
 
-function init(R::RGE)
+function Copy(R::RGE)::RGE
+    S = RGE(0.,0.,0.,0.,0.,0.,0.,0.)
+    S.t = R.t
+    S.lH = R.lH
+    S.yt = R.yt
+    S.g3 = R.g3
+    S.g2 = R.g2
+    S.g1 = R.g1
+    S.phi = R.phi
+    S.G = R.G
+    return S
+end
+
+function init(mt::Float64)::RGE
+    R = RGE(0.,0.,0.,0.,0.,0.,0.,0.)
     R.t = 0.
 	R.lH = 0.12604 + 0.00206 * (MH - 125.15) - 0.00004 * (mt - 173.34)
 	R.yt = (0.93690 + 0.00556 * (mt - 173.34) - 0.00003 * (MH - 125.15) - 0.00042 * (alphasMZ - 0.1184) / 0.0007)
@@ -27,6 +41,7 @@ function init(R::RGE)
 	R.g1 = 0.35830 + 0.00011 * (mt - 173.34) - 0.00020 * (MW - 80.384) / 0.014
 	R.phi = sqrt(2.) / R.yt * mt * exp(R.t)
 	R.G = 1.
+    return R
 end
 
 function MakeBeta(γ)
@@ -43,7 +58,7 @@ function Running(R::RGE, mt::Float64, xi::Float64)
     # 1-loop Beta Function
     β1_g1 = (81 + sh) / 12 * R.g1^3
     β1_g2 = -(39 - sh) / 12 * R.g2^3
-    β1_g3 = -7 * g3^3
+    β1_g3 = -7 * R.g3^3
     β1_lH = 6(1 + 3sh^2) * R.lH^2 + 12R.lH * R.yt^2 - 6R.yt^4 - 3R.lH * (3R.g2^2 + R.g1^2) + 3 / 8 * (2R.g2^4 + (R.g1^2 + R.g2^2)^2)
     β1_yt = R.yt * ((23 / 6 + 2 / 3 * sh) * R.yt^2 - (8 * R.g3^2 + 9 / 4 * R.g2^2 + 17 / 12 * R.g1^2))
     # β1_xi = (6(1+sh)*R.lH + 6*R.yt^2-3/2*(R.g1^2+3*R.g2^2))*(R.xi + 1/6)
@@ -72,4 +87,16 @@ function Running(R::RGE, mt::Float64, xi::Float64)
     R.t += h
     R.phi = sqrt(2)/R.yt*mt*exp(R.t)
     R.G -= h*γ/(1+γ)
+end
+
+# Run generate Array of RGE
+function Run(mt::Float64, xi::Float64)::Array{RGE}
+    R = init(mt)
+    Container = Array{RGE}(length(0:h:N))
+    Container[1] = Copy(R)
+    for i = 2:length(0:h:N)
+        Running(R, mt, xi)
+        Container[i] = Copy(R)
+    end
+    return Container
 end
