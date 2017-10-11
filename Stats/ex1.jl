f20cba94a59ceab469ab6f8daf243ae89947b2d2
Axect <edeftg@gmail.com>
2017-10-11 18:46:26 +0900

    Add Cov

--- Stats/ex1.jl
+++ Stats/ex1.jl
@@ -1,8 +1,10 @@
 include("src/prob.jl")
 
 A = collect(1.:1.:5.)
+B = collect(5.:-1.:1.)
 
 println(A)
 println("Mean: ", Mean(A))
 println("Var: ", Var(A))
 println("Std: ", Std(A))
+println("Cov: ", Cov(A,B))
--- Stats/src/prob.jl
+++ Stats/src/prob.jl
@@ -9,7 +9,7 @@ end
 function Var(A::Array{Float64})::Float64
     # Sample Mean
     m = Mean(A)
-    s = 0
+    s = 0.
     for x in A
         s += (x - m)^2
     end
@@ -19,4 +19,15 @@ end
 
 function Std(A::Array{Float64})::Float64
     return sqrt(Var(A))
+end
+
+function Cov(A::Array{Float64}, B::Array{Float64})::Float64
+    ma = Mean(A)
+    mb = Mean(B)
+    s = 0.
+    for i = 1:length(A)
+        s += (A[i] - ma)*(B[i] - mb)
+    end
+    s = s / (length(A)-1)
+    return s
 end
