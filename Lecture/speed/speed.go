129f1d9084996b6530ed9293847dde09759ec6f6
Tae Geun, Kim <edeftg@gmail.com>
2018-09-23 19:25:12 +0900

    Go Rust

--- Lecture/speed/speed.go
+++ Lecture/speed/speed.go
@@ -1,19 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
 func main() {
-	start := time.Now()
 	var A [1E+08]int
 	a := &A
 	for i := range A {
 		a[i] = i * i
 	}
-	elapsed := time.Since(start)
-	fmt.Println(elapsed)
 }
 
 // Output : 243.41662ms = 0.243 sec
