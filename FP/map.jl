bed65b60568b03ddbf907bb7ec835cd7d11330cc
Axect <axect@outlook.kr>
2020-09-21 19:36:20 +0900

    New bench

--- FP/map.jl
+++ FP/map.jl
@@ -5,29 +5,33 @@ function mapTest(n::Int)::Array{Int} # 0.24 s
     return A
 end
 
-function reduceTest(n::Int)::Int # 0.03 s
-    A = reduce(+, 0, 1:n)
+function reduceTest(n::Int)::Int # 0.005 s
+    A = reduce(+, 1:n)
     return A
 end
 
-function foldlTest(n::Int)::Int # 0.013 s
-    A = foldl(+, 0, 1:n)
+function foldlTest(n::Int)::Int # 0.011 s
+    A = foldl(+, 1:n)
     return A
 end
 
 
-function forTest1(n::Int)::Array{Int} # 0.37 s
-    A = Array{Int}(n)
+function forTest1(n::Int)::Array{Int} # 0.68 s
+    A = zeros(n)
     for i = 1:n
         A[i] = i^2
     end
     return A
 end
 
-function forTest2(n::Int)::Int # 0.017 s
+function forTest2(n::Int)::Int # 0.006 s
     s = 0
     for i=1:n
         s += i
     end
     return s
+end
+
+function sumTest(n::Int)::Int # 0.007 s
+    sum(1:n)
 end
