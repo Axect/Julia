040ac0c468b9eb83b2b23770071bb8f361366701
Axect <axect@outlook.kr>
2020-09-21 19:27:59 +0900

    Fix syntax

--- Tutorial/DataScience/gmle.jl
+++ Tutorial/DataScience/gmle.jl
@@ -3,16 +3,16 @@ using Distributions
 function Genmle()
   d = Normal(3,1)
   x = rand(d, 300, 2);
-  μ = Vector{Float64}(2)
+  μ = zeros(2)
   for i = 1:2
     μ[i] = mean(x[:,i])
   end
-  x_new = Array{Float64}(300, 2)
+  x_new = similar(x)
   for i = 1:2
     # Cool Vectorization
     x_new[:,i] = x[:,i].-μ[i]
   end
-  Σ = 1/299.*(x_new'*x_new)
+  Σ = 1/299 * (x_new'*x_new)
   println("Mean=", μ)
   println("Cov=", Σ)
   println("Original Cov=", cov(x))
