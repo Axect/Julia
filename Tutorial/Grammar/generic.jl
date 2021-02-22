1084fca69292424ae1021b141a8d445ef3204f12
Axect <axect@outlook.kr>
2021-02-23 02:37:16 +0900

    Hmm

--- Tutorial/Grammar/generic.jl
+++ Tutorial/Grammar/generic.jl
@@ -5,3 +5,17 @@ end
 hello([1,2,3])
 
 hello([1 2;3 4])
+
+struct Point{T, N}
+    data::Array{T, N}
+end
+
+function Point(x::U) where {T <: Number, U <: AbstractRange{T}}
+    return Point{T, 1}(collect(x))
+end
+
+p = Point([1,2,3])
+q = Point(1:10)
+
+println(p)
+println(q)
