0b68626b138900eee902950cfdbf08e9b2492eb9
axect <axect@outlook.kr>
2021-09-30 03:51:23 +0900

    Flux Basics Completed!

---
+++ Flux/download.sh
@@ -0,0 +1,3 @@
+wget -O data/apples.dat https://github.com/JuliaAcademy/JuliaAcademyData.jl/raw/main/courses/Deep%20learning%20with%20Flux/data/apples.dat
+
+wget -O data/bananas.dat https://github.com/JuliaAcademy/JuliaAcademyData.jl/raw/main/courses/Deep%20learning%20with%20Flux/data/bananas.dat
